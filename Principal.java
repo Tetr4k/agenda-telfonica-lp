504d62df8cb18121c543a09e0dec054b172ae090
Gabriel Ribeiro <35122236+Tetr4k@users.noreply.github.com>
2021-08-14 18:08:19 +0000

    Cria arquivos base

--- Principal.java
+++ Principal.java
@@ -4,6 +4,13 @@ public class Principal
 {
 	public static void main(String[] args)
 	{
-		/*Implementação*/
+/*
+Inserir o Contato "Fulano", 99999999, "Rua A", "UFF";
+Inserir o Contato "Ciclano", 88888888, "Rua B", "Cederj";
+Inserir o Contato "Beltrano", 88889999, "Rua C", "Infância";
+Inserir o Contato "Fulano", 77777777, "Rua D", none;
+Remover o Contato "Ciclano";
+Listar o conteudo da Agenda;
+*/
 	}
 }
