b9609a8064c4b28d9af14029f0a48d7de6eba0cd
Gabriel <g2012ribeiro@gmail.com>
2021-08-17 12:04:42 -0300

    Implementação base
    * Inserir o Contato "Fulano", 99999999, "Rua A", "UFF";
    * Inserir o Contato "Ciclano", 88888888, "Rua B", "Cederj";
    * Inserir o Contato "Beltrano", 88889999, "Rua C", "Infância";
    * Inserir o Contato "Fulano", 77777777, "Rua D", none;
    * Remover o Contato "Ciclano";
    * Listar o conteudo da Agenda;

--- Principal.java
+++ Principal.java
@@ -4,13 +4,12 @@ public class Principal
 {
 	public static void main(String[] args)
 	{
-/*
-Inserir o Contato "Fulano", 99999999, "Rua A", "UFF";
-Inserir o Contato "Ciclano", 88888888, "Rua B", "Cederj";
-Inserir o Contato "Beltrano", 88889999, "Rua C", "Infância";
-Inserir o Contato "Fulano", 77777777, "Rua D", none;
-Remover o Contato "Ciclano";
-Listar o conteudo da Agenda;
-*/
+		Agenda agenda1 = new Agenda();
+		agenda1.inserir("Fulano", 99999999, "Rua A", "UFF");
+		agenda1.inserir("Ciclano", 88888888, "Rua B", "Cederj");
+		agenda1.inserir("Beltrano", 88889999, "Rua C", "Infância");
+		agenda1.inserir("Fulano", 77777777, "Rua D");
+		agenda1.remover("Ciclano");
+		System.out.println(agenda1.toString());
 	}
 }
