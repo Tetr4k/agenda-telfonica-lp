fc12aa76742845bc5eebb6dac7f41b61e009c55f
Gabriel <g2012ribeiro@gmail.com>
2021-08-17 18:16:51 -0300

    Inicio do menu

--- Principal.java
+++ Principal.java
@@ -1,4 +1,5 @@
 /*Importações*/
+import java.util.Scanner;
 
 public class Principal
 {
@@ -6,15 +7,34 @@ public class Principal
 		for(int x=0; x<25; x++) System.out.println();
 	}
 
+	static void imprimeMenu(){//Inserir, Remover, Atualizar, buscar, salvar, recuperar
+		System.out.print("=============\n"+
+						 "1. Buscar;\n"+
+						 "2. Inserir;\n"+
+						 "3. Atualizar;\n"+
+						 "4. Remover;\n"+
+						 "5. Salvar;\n"+
+						 "6. Recuperar;\n"+
+						 "7. Sair\n"+
+						 "=============\n"+
+						 ">");
+	}
+
 	public static void main(String[] args)
 	{
-		Agenda agenda1 = new Agenda();
-		agenda1.inserir("Fulano", 99999999, "Rua A", "UFF");
-		agenda1.inserir("Ciclano", 88888888, "Rua B", "Cederj");
-		agenda1.inserir("Beltrano", 88889999, "Rua C", "Infância");
-		agenda1.inserir("Fulano", 77777777, "Rua D");
-		agenda1.remover("Ciclano");
-		limpaTela();
-		System.out.println(agenda1.toString());
+		Scanner leitor = new Scanner(System.in);
+		Agenda agenda = new Agenda();
+		agenda.inserir("Fulano", 99999999, "Rua A", "UFF");
+		agenda.inserir("Ciclano", 88888888, "Rua B", "Cederj");
+		agenda.inserir("Beltrano", 88889999, "Rua C", "Infância");
+		agenda.inserir("Fulano", 77777777, "Rua D");
+		agenda.remover("Ciclano");
+		while(true){
+			limpaTela();
+			System.out.println(agenda.toString());
+			imprimeMenu();
+			int opcao = leitor.nextInt();
+		}
+		leitor.close();
 	}
 }
