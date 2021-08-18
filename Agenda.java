d2d20c16c2b6092f3af27b2efe79ce40c41da455
Gabriel <g2012ribeiro@gmail.com>
2021-08-18 16:10:24 -0300

    Cria basico da Agenda

--- Agenda.java
+++ Agenda.java
@@ -1,4 +1,21 @@
+import java.util.ArrayList;
+
 public class Agenda
 {
+    private ArrayList<Contato> contatos;
 
+    public Agenda()
+    {
+        this.contatos = new ArrayList<Contato>();
+    }
+
+    public String toString()
+    {
+        String texto = "AGENDA\n\n";
+        for(Contato contato : this.contatos)
+        {
+            texto = texto.concat(contato.toString());
+        }
+        return texto;
+    }
 }
