2f27c0038adb1090e6420578edb0f580453dabc5
Gabriel <g2012ribeiro@gmail.com>
2021-08-18 15:59:21 -0300

    Criou basico do Contato

--- Contato.java
+++ Contato.java
@@ -1,4 +1,21 @@
 public class Contato
 {
-	
+	private String nome, endereco, relacao;
+    private int numero;
+
+    public Contato(String nome, int numero, String endereco, String relacao)
+    {
+        this.nome       = nome;
+        this.numero     = numero;
+        this.endereco   = endereco;
+        this.relacao    = relacao;
+    }
+
+    public String toString()
+    {
+        return "Nome: "+this.nome+"\n"+
+               "Telefone: "+this.numero+"\n"+
+               "Endereco: "+this.endereco+"\n"+
+               "Relacao: "+this.relacao;
+    }
 }
