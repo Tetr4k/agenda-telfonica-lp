eef031e535f6c9825d97818715f0e0224ca11356
Gabriel <g2012ribeiro@gmail.com>
2021-08-19 13:25:05 -0300

    Cria função esperaEnter()

--- AgendaManager.java
+++ AgendaManager.java
@@ -1,11 +1,27 @@
+import java.io.IOException;
 import java.util.Scanner;
 
 public class AgendaManager {
-	static void limpaTela(){
+	static void limpaTela()
+	{
 		for(int x=0; x<25; x++) System.out.println();
 	}
 
-	static void imprimeMenu(){
+	static void esperaEnter()
+	{
+		try
+		{
+			System.out.println("Pressione enter para continuar . . .");
+			System.in.read();
+		}
+		catch(IOException e)
+		{
+			System.out.println(e);
+		}
+	}
+
+	static void imprimeMenu()
+	{
 		System.out.print("=====MENU=====\n"+
 						 "1 - Buscar;\n"+
 						 "2 - Inserir;\n"+
@@ -30,68 +46,93 @@ public class AgendaManager {
 			System.out.println(agenda);
 			imprimeMenu();
 			opcao = leitorInt.nextInt();
+			limpaTela();
 			switch(opcao)
 			{
 				case 1:
 					System.out.println("Qual o nome do contato?");
 					nome = leitorString.nextLine();
+					limpaTela();
 					contato = agenda.buscar(nome);
 					if(contato != null)
 						System.out.println(contato);
 					else
 						System.out.println("Contato não encontrado");
+					esperaEnter();
+					limpaTela();
 					break;
 				case 2:
 					System.out.println("Qual o nome do contato?");
 					nome = leitorString.nextLine();
+					limpaTela();
 					System.out.println("Qual o numero do contato?");
 					numero = leitorInt.nextInt();
+					limpaTela();
 					System.out.println("Qual o endereco do contato?");
 					endereco = leitorString.nextLine();
+					limpaTela();
 					System.out.println("Qual a relação do contato?");
 					relacao = leitorString.nextLine();
+					limpaTela();
 					if(agenda.inserir(nome, numero, endereco, relacao))
 						System.out.println("Inserido com Sucesso");
 					else
 						System.out.println("Alterado com Sucesso");
+					esperaEnter();
+					limpaTela();
 					break;
 				case 3:
 					System.out.println("Qual o nome do contato?");
 					nome = leitorString.nextLine();
+					limpaTela();
 					System.out.println("Qual o numero do contato?");
 					numero = leitorInt.nextInt();
+					limpaTela();
 					System.out.println("Qual o endereco do contato?");
 					endereco = leitorString.nextLine();
+					limpaTela();
 					System.out.println("Qual a relação do contato?");
 					relacao = leitorString.nextLine();
+					limpaTela();
 					if(agenda.alterar(nome, numero, endereco, relacao))
 						System.out.println("Alterado com Sucesso");
 					else
 						System.out.println("Contato inexistente");
+					esperaEnter();
+					limpaTela();
 					break;
 				case 4:
 					System.out.println("Qual o nome do contato?");
 					nome = leitorString.nextLine();
+					limpaTela();
 					if(agenda.remover(nome))
 						System.out.println("Contato removido com sucesso");
 					else
 						System.out.println("Contato não esta na agenda");
+					esperaEnter();
+					limpaTela();
 					break;
 				case 5:
 					System.out.println("Qual o nome do arquivo?");
 					nomeArquivo = leitorString.nextLine();
+					limpaTela();
 					if(agenda.salvar(nomeArquivo))
 						System.out.println("Salvo com sucesso");
 					else
 						System.out.println("Erro ao salvar");
+					esperaEnter();
+					limpaTela();
 					break;
 				case 6:
 					System.out.println("Qual o nome do arquivo?");
 					nomeArquivo = leitorString.nextLine();
+					limpaTela();
 					if(agenda.recuperar(nomeArquivo))
 						System.out.println("Recuperado com sucesso");
 					else
 						System.out.println("Erro ao salvar");
+					esperaEnter();
+					limpaTela();
 					break;
 			}
 		}while(opcao != 7);
